76619945b3f2c1bcfa18586b0c4602133e7065ba
Maxwell <joey-hzpfywtd@hotmail.com>
2024-07-15 20:07:50 +0300

    Finalise go side

--- gfcm.go
+++ gfcm.go
@@ -1,20 +1,16 @@
 package goffmpegconvertermicroservice
 
 import (
-	"context"
-	"encoding/json"
+	"bufio"
 	"errors"
-	"flag"
 	"fmt"
-	"google.golang.org/grpc"
-	"google.golang.org/protobuf/proto"
 	"io"
 	"log"
-	"math"
 	"net"
 	"os"
-	"sync"
-	"time"
+	"strings"
+
+	"google.golang.org/grpc"
 
 	pb "github.com/g13n4/go-ffmpeg-converter-microservice/video-converter"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
@@ -22,16 +18,19 @@ import (
 
 var FILE_HANDLER_MAP = map[int64]*os.File{}
 
+type videoConverterServer struct {
+	pb.UnimplementedVideoConverterServer
+	videoToConvertChunks []*pb.VideoToConvert
+	convertedVideoChunks []*pb.ConvertedVideo
+}
+
 func WriteBytesToFile(fileName string, videoId int64, data []byte) error {
 	// Check if file exists. If it does we append bytes and if not we create a new one
 	handler, ok := FILE_HANDLER_MAP[videoId]
 
 	if ok {
-		if _, err := handler.Write(data); err != nil {
-			handler.Close()
-			delete(FILE_HANDLER_MAP, videoId)
-			return err
-		}
+		_, handlerErr := handler.Write(data)
+		return handlerErr
 	} else {
 		file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
@@ -39,30 +38,22 @@ func WriteBytesToFile(fileName string, videoId int64, data []byte) error {
 			return err
 		}
 
-		if _, err := file.Write(data); err != nil {
-			file.Close()
-			removeFileError := os.Remove(fileName)
-			delete(FILE_HANDLER_MAP, videoId)
-
-			if removeFileError != nil {
-				return removeFileError
-			}
-
-			return err
+		_, writeErr := file.Write(data)
+		if writeErr == nil {
+			FILE_HANDLER_MAP[videoId] = file
+			return nil
 		}
-
-		FILE_HANDLER_MAP[videoId] = file
-
-		return nil
-
+		return writeErr
 	}
-	return nil
+
 }
 
-type videoConverterServer struct {
-	pb.UnimplementedVideoConverterServer
-	videoToConvertChunks []*pb.VideoToConvert
-	convertedVideoChunks []*pb.ConvertedVideo
+func TransformVideo(fileInput string, fileOutput string, fileKwargs map[string]interface{}) error {
+	err := ffmpeg.Input(fileInput).
+		Output(fileOutput, fileKwargs).
+		OverWriteOutput().ErrorToStdOut().Run()
+
+	return err
 }
 
 func (vcs *videoConverterServer) ConvertVideo(stream pb.VideoConverter_ConvertVideoServer) error {
@@ -79,12 +70,12 @@ func (vcs *videoConverterServer) ConvertVideo(stream pb.VideoConverter_ConvertVi
 		// and we can safely skip next chunks
 
 		vi := in.VideoInfo
-		handler, ok := FILE_HANDLER_MAP[vi.VideoId]
-		if vi.chunkIndex != 1 && !ok {
-			// TODO: replace the error with a response
-			sendError := stream.Send(vcs.ConvertedVideo{
-				success: false,
-				error:   "There is something wrong with the file",
+		_, ok := FILE_HANDLER_MAP[vi.VideoId]
+		if in.ChunkIndex != 1 && !ok {
+			errorMessage := "Redundant Error"
+			sendError := stream.Send(&pb.ConvertedVideo{
+				Sucess: false,
+				Error:  &errorMessage,
 			})
 			if sendError != nil {
 				return sendError
@@ -96,47 +87,95 @@ func (vcs *videoConverterServer) ConvertVideo(stream pb.VideoConverter_ConvertVi
 
 		writingErr := WriteBytesToFile(fileNameInput, vi.VideoId, in.VideoFeed)
 
+		// something went wrong so we remove the file from our file system
+		errorsList := []string{}
 		if writingErr != nil {
-			handler, ok := FILE_HANDLER_MAP[videoId] // check if file exists. If it does we remove it
+			errorsList = append(errorsList, writingErr.Error())
+			handler, ok := FILE_HANDLER_MAP[vi.VideoId] // check if file exists. If it does we remove it
 			if ok {
-				delete(FILE_HANDLER_MAP, videoId)
+				delete(FILE_HANDLER_MAP, vi.VideoId)
+			}
+			closeError := handler.Close()
+			if closeError != nil {
+				errorsList = append(errorsList, closeError.Error())
 			}
 
 			_, fileStatsErr := os.Stat(fileNameInput)
+
 			if errors.Is(fileStatsErr, os.ErrNotExist) {
+				removeFileError := os.Remove(fileNameInput)
 
-				removeFileError := os.Remove(fileName)
+				if removeFileError != nil {
+					errorsList = append(errorsList, removeFileError.Error())
+				}
 			}
-
-			if removeFileError != nil {
-				return removeFileError
-			}
-
-			if removeFileError != nil {
-				return removeFileError
-			}
-
 		}
 
+		// everything is great. Time to transform it
+		if in.LastChuck {
+			transformationError := TransformVideo(fileNameInput, fileNameOutput, in.ConvertionSettings)
+
+			// ready to send it back
+			if transformationError == nil {
+				outputFile, outputFileErr := os.OpenFile(fileNameOutput, os.O_RDONLY|os.O_WRONLY, 0644)
+
+				if outputFileErr != nil {
+					errorsList = append(errorsList, outputFileErr.Error())
+				} else {
+					readBuffer := make([]byte, 0, 1024*4)
+					bufReader := bufio.NewReader(outputFile)
+					for {
+						n, BRerr := bufReader.Read(readBuffer[:cap(readBuffer)])
+						readBuffer = readBuffer[:n] // take only existing bytes
+						if n == 0 {
+							if BRerr == nil {
+								continue
+							}
+							if BRerr == io.EOF {
+								break
+							}
+						}
+
+						if BRerr != nil && BRerr != io.EOF {
+							errorsList = append(errorsList, BRerr.Error())
+							break
+						}
+
+						sendError := stream.Send(&pb.ConvertedVideo{
+							Sucess:    false,
+							VideoFeed: readBuffer,
+						})
+
+						if sendError != nil {
+							errorsList = append(errorsList, sendError.Error())
+							break
+						}
+
+					}
+				}
+
+			}
+		}
+
+		// return a concataned list of errors
+		if len(errorsList) > 0 {
+			errorMessages := strings.Join(errorsList, " Error: ")
+			sendError := stream.Send(&pb.ConvertedVideo{
+				Sucess: false,
+				Error:  &errorMessages,
+			})
+			if sendError != nil {
+				return sendError
+			}
+		}
 	}
-}
-
-func TransformVideo(fileInput string, fileOutput string, fileKwargs ffmpeg.KwArgs) (string, error) {
-
-	err := ffmpeg.Input(fileInput).
-		Output(fileOutput, fileKwargs).
-		OverWriteOutput().ErrorToStdOut().Run()
-
-	if err != nil {
-		return "There is an error!", err
-	}
-
-	return fileOutput, nil
 }
 
 func main() {
-	var port string
-	flag.StringVar(&port, "port", "50051", "The server port")
+	port := os.Getenv("GRPC_PORT")
+	if len(port) == 0 {
+		port = "50051"
+	}
 
 	lis, err := net.Listen("tcp", port)
 
@@ -145,181 +184,6 @@ func main() {
 	}
 	grpcService := grpc.NewServer()
 
-	pb.RegisterVideoConverterServer
-}
-
-type routeGuideServer struct {
-	pb.UnimplementedRouteGuideServer
-	savedFeatures []*pb.Feature // read-only after initialized
-
-	mu         sync.Mutex // protects routeNotes
-	routeNotes map[string][]*pb.RouteNote
-}
-
-// GetFeature returns the feature at the given point.
-func (s *routeGuideServer) GetFeature(ctx context.Context, point *pb.Point) (*pb.Feature, error) {
-	for _, feature := range s.savedFeatures {
-		if proto.Equal(feature.Location, point) {
-			return feature, nil
-		}
-	}
-	// No feature was found, return an unnamed feature
-	return &pb.Feature{Location: point}, nil
-}
-
-// ListFeatures lists all features contained within the given bounding Rectangle.
-func (s *routeGuideServer) ListFeatures(rect *pb.Rectangle, stream pb.RouteGuide_ListFeaturesServer) error {
-	for _, feature := range s.savedFeatures {
-		if inRange(feature.Location, rect) {
-			if err := stream.Send(feature); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
-}
-
-// RecordRoute records a route composited of a sequence of points.
-//
-// It gets a stream of points, and responds with statistics about the "trip":
-// number of points,  number of known features visited, total distance traveled, and
-// total time spent.
-func (s *routeGuideServer) RecordRoute(stream pb.RouteGuide_RecordRouteServer) error {
-	var pointCount, featureCount, distance int32
-	var lastPoint *pb.Point
-	startTime := time.Now()
-	for {
-		point, err := stream.Recv()
-		if err == io.EOF {
-			endTime := time.Now()
-			return stream.SendAndClose(&pb.RouteSummary{
-				PointCount:   pointCount,
-				FeatureCount: featureCount,
-				Distance:     distance,
-				ElapsedTime:  int32(endTime.Sub(startTime).Seconds()),
-			})
-		}
-		if err != nil {
-			return err
-		}
-		pointCount++
-		for _, feature := range s.savedFeatures {
-			if proto.Equal(feature.Location, point) {
-				featureCount++
-			}
-		}
-		if lastPoint != nil {
-			distance += calcDistance(lastPoint, point)
-		}
-		lastPoint = point
-	}
-}
-
-// RouteChat receives a stream of message/location pairs, and responds with a stream of all
-// previous messages at each of those locations.
-func (s *routeGuideServer) RouteChat(stream pb.RouteGuide_RouteChatServer) error {
-	for {
-		in, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			return err
-		}
-		key := serialize(in.Location)
-
-		s.mu.Lock()
-		s.routeNotes[key] = append(s.routeNotes[key], in)
-		// Note: this copy prevents blocking other clients while serving this one.
-		// We don't need to do a deep copy, because elements in the slice are
-		// insert-only and never modified.
-		rn := make([]*pb.RouteNote, len(s.routeNotes[key]))
-		copy(rn, s.routeNotes[key])
-		s.mu.Unlock()
-
-		for _, note := range rn {
-			if err := stream.Send(note); err != nil {
-				return err
-			}
-		}
-	}
-}
-
-// loadFeatures loads features from a JSON file.
-func (s *routeGuideServer) loadFeatures(filePath string) {
-	var data []byte
-	if filePath != "" {
-		var err error
-		data, err = os.ReadFile(filePath)
-		if err != nil {
-			log.Fatalf("Failed to load default features: %v", err)
-		}
-	} else {
-		data = exampleData
-	}
-	if err := json.Unmarshal(data, &s.savedFeatures); err != nil {
-		log.Fatalf("Failed to load default features: %v", err)
-	}
-}
-
-func toRadians(num float64) float64 {
-	return num * math.Pi / float64(180)
-}
-
-// calcDistance calculates the distance between two points using the "haversine" formula.
-// The formula is based on http://mathforum.org/library/drmath/view/51879.html.
-func calcDistance(p1 *pb.Point, p2 *pb.Point) int32 {
-	const CordFactor float64 = 1e7
-	const R = float64(6371000) // earth radius in metres
-	lat1 := toRadians(float64(p1.Latitude) / CordFactor)
-	lat2 := toRadians(float64(p2.Latitude) / CordFactor)
-	lng1 := toRadians(float64(p1.Longitude) / CordFactor)
-	lng2 := toRadians(float64(p2.Longitude) / CordFactor)
-	dlat := lat2 - lat1
-	dlng := lng2 - lng1
-
-	a := math.Sin(dlat/2)*math.Sin(dlat/2) +
-		math.Cos(lat1)*math.Cos(lat2)*
-			math.Sin(dlng/2)*math.Sin(dlng/2)
-	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
-
-	distance := R * c
-	return int32(distance)
-}
-
-func inRange(point *pb.Point, rect *pb.Rectangle) bool {
-	left := math.Min(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
-	right := math.Max(float64(rect.Lo.Longitude), float64(rect.Hi.Longitude))
-	top := math.Max(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
-	bottom := math.Min(float64(rect.Lo.Latitude), float64(rect.Hi.Latitude))
-
-	if float64(point.Longitude) >= left &&
-		float64(point.Longitude) <= right &&
-		float64(point.Latitude) >= bottom &&
-		float64(point.Latitude) <= top {
-		return true
-	}
-	return false
-}
-
-func serialize(point *pb.Point) string {
-	return fmt.Sprintf("%d %d", point.Latitude, point.Longitude)
-}
-
-func newServer() *routeGuideServer {
-	s := &routeGuideServer{routeNotes: make(map[string][]*pb.RouteNote)}
-	s.loadFeatures(*jsonDBFile)
-	return s
-}
-
-func oldMain() {
-	flag.Parse()
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-	var opts []grpc.ServerOption
-	grpcServer := grpc.NewServer(opts...)
-	pb.RegisterRouteGuideServer(grpcServer, newServer())
-	grpcServer.Serve(lis)
+	pb.RegisterVideoConverterServer(grpcService, &videoConverterServer{})
+	grpcService.Serve(lis)
 }
